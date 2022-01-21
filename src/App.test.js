229abfb8a68dc30fb3abb40212e18c1adf98480b
dikshya <dixyacharya@gmail.com>
2022-01-20 23:45:52 -0700

    deleted files

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Hello World link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, World/i);
-  expect(linkElement).toBeInTheDocument();
-});
