e4bb421323a136866a5dc19a6d5d17d6113478f5
dikshya <dixyacharya@gmail.com>
2022-01-22 00:14:41 -0700

    new files added

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App(){
+    return (
+        <div>
+            <h1>Hello from the App component</h1>
+            <p> Hi</p>
+        </div>        
+        
+    );
+}
+
+export default App
